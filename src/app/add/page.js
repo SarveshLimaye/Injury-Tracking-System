2f3fd092fd768368dc7fdf77c8bb43ecf07a950c
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-11-05 19:06:25 +0530

    Add report ui done

---
+++ src/app/add/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import BodyMap from "../components/BodyMap/BodyMap";
+
+export default function AddReport() {
+  return (
+    <div>
+      <BodyMap />
+    </div>
+  );
+}
